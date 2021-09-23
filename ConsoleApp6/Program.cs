a7b02422992b46ffc9c5314ab98ad90705c97bed
Kiberio <dhiroshi3@gmail.com>
2021-09-22 21:10:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração da variavel e solicitando o numero de pessoas
+            Console.WriteLine("Digite a quantidade de pessoas: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+
+            Console.WriteLine("Digite a altura das pessoas: ");
+
+            //for para registrar as alturas na memoria
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            //Declaração da variavel soma
+            double soma = 0.0;
+
+            //for para somar todas as alturas
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+            //Calculo da media
+            double media = soma / n;
+            
+            //Resultados
+            Console.WriteLine("soma: " + soma);
+            Console.WriteLine("Media: " + media);
+            Console.ReadKey();
+        }
+    }
+}
